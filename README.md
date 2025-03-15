96040204026e229739d2c5cfb42409b91652c288
Peter Fetterer <kb3gtn@gmail.com>
2025-03-15 04:16:05 -0400

    Initial Checking to GIT

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # pico_dev_cli
-Setup / Build RP PICO toolchain cli on linux
+
+Build and setup toolchain and sdk to work via cmake on the linux command line.
+All component will be placed at the directory level you run the script from.
+
+This script will pull pico-sdk from git and build picotool
+This tool will also pull jimtcl and openocd and build the jtag solution as well.
+
+help full scripts and tool binaries are located in pico_tools/bin
+
+This script is a prototype to automate the setup process. Still a work in progress..
+
+comes with a simple blinky example you can try and build once the install is complete to test
+if the build environment is working correctly.
---
+++ build_pico_devtools.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+# This script pull all the sources needed to build and setup the RP 2040/2350 PICO development environment and toolchains.
+# Stuff will be installed in the directory you are running this script.
+
+basedir="$PWD"
+echo "Using basedir = ${basedir}"
+
+# Include Dependency packages
+echo "** Apt install dependencies **"
+if [  -n "$(uname -a | grep Ubuntu)" ]; then
+    sudo apt install automake autoconf build-essential texinfo libtool libhidapi-dev libusb-1.0-0-dev
+else
+    echo "  Skipped, not supported distro. Will have to do this manually"
+    #arch distros: might need libhid libusb
+fi 
+
+# make pico_tools directory
+mkdir -p ${basedir}/pico_tools
+
+#tell pkg_config we have a special directory to consider for builds here..
+#need for openocd build..
+export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:${basedir}/pico_tools/lib/pkgconfig"
+
+# Pull pico_sdk
+echo "** Pulling RP Pico SDK **"
+git clone https://github.com/raspberrypi/pico-sdk.git pico_sdk
+cd pico_sdk
+git submodule update --init
+mkdir build
+cd build
+cmake ../
+make -j16
+cp ./_deps/picotool/picotool ${basedir}/pico_tools/bin
+cp ./_deps/picotool-src/udev/99-picotool.rules ${basedir}
+cd ../../
+
+# Build JimTCL for the system
+echo "** Building JimTCL for OpenOCD **"
+git clone https://github.com/msteveb/jimtcl.git jimtcl_build
+cd jimtcl_build
+./configure --prefix=${basedir}/pico_tools
+make -j32
+make install
+
+cd ..
+
+# Build OpenOCD
+echo "** Pull and build OpenOCD **"
+git clone git://git.code.sf.net/p/openocd/code openocd_build
+cd openocd_build
+./bootstrap
+./configure --enable-cmsis-dap --prefix=${basedir}/pico_tools
+make -j32
+make install
+
+cd ..
+
+# Create enviroment variables script
+echo "** creating env_setup.sh -- source this to set environment variables.."
+cat << EOF > env_setup.sh
+#!/bin/bash
+# source env_setup.sh 
+export PICO_BASE=/home/kb3gtn/sandbox/rp_pico_projects
+export PICO_SDK_PATH=${PICO_BASE}/pico_sdk
+export PATH=$PATH:${PICO_BASE}/pico_tools/bin
+EOF
+
+# Install cross-compiler 
+echo "** Install arm cross compiler **"
+echo " Need to do manual install if not already installed before building stuff.. "
+echo " Arch Distros:"
+echo "   arm-none-eabi-binutls, arm-none-eabi-gcc arm-none-eabi-gdb arm-none-eabi-newlib "
+echo ""
+echo " Ubuntu/Debian Distros:"
+echo "   binutils-arm-none-eabi gcc-arm-none-eabi newlib-arm-none-eabi gdb-multiarch"
+echo ""
+
+echo "** UDEV Rules **"
+echo " install 99-picotool.rules /etc/udev/rules.d then udevadm control --reload-rules "
+echo "!! Script Complete.. !!"
+
