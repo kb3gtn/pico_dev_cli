463ab2b49cd2a3abfdfc50ad8c30c1f2b5ae35b0
Peter Fetterer <kb3gtn@gmail.com>
2025-03-15 16:51:40 -0400

    Update build_pico_devtools.sh
    WIP

--- build_pico_devtools.sh
+++ build_pico_devtools.sh
@@ -14,34 +14,39 @@ else
     #arch distros: might need libhid libusb
 fi 
 
-# make pico_tools directory
-mkdir -p ${basedir}/pico_tools
+# make tools directory
+mkdir -p ${basedir}/tools
 
 #tell pkg_config we have a special directory to consider for builds here..
 #need for openocd build..
-export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:${basedir}/pico_tools/lib/pkgconfig"
+export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:${basedir}/tools/lib/pkgconfig"
 
 # Pull pico_sdk
 echo "** Pulling RP Pico SDK **"
-git clone https://github.com/raspberrypi/pico-sdk.git pico_sdk
-cd pico_sdk
+git clone https://github.com/raspberrypi/pico-sdk.git pico-sdk
+cd pico-sdk
 git submodule update --init
+cd ..
+
+mkdir builds
+cd builds
+
+echo "** build PICOTools **"
+git clone https://github.com/raspberrypi/picotool.git picotool_build
+cd picotool_build
 mkdir build
 cd build
 cmake ../
-make -j16
-cp ./_deps/picotool/picotool ${basedir}/pico_tools/bin
-cp ./_deps/picotool-src/udev/99-picotool.rules ${basedir}
-cd ../../
+make -j8
+cp picotool ../../../pico_tools/bin
 
 # Build JimTCL for the system
 echo "** Building JimTCL for OpenOCD **"
 git clone https://github.com/msteveb/jimtcl.git jimtcl_build
 cd jimtcl_build
-./configure --prefix=${basedir}/pico_tools
-make -j32
+./configure --prefix=${basedir}/tools
+make -j8
 make install
-
 cd ..
 
 # Build OpenOCD
@@ -49,10 +54,12 @@ echo "** Pull and build OpenOCD **"
 git clone git://git.code.sf.net/p/openocd/code openocd_build
 cd openocd_build
 ./bootstrap
-./configure --enable-cmsis-dap --prefix=${basedir}/pico_tools
-make -j32
+./configure --enable-cmsis-dap --prefix=${basedir}/tools --disable-werror
+make -j8
 make install
+cd ..
 
+# leave builds dir
 cd ..
 
 # Create enviroment variables script
@@ -63,7 +70,7 @@ cat << EOF > env_setup.sh
 export PICO_BASE=${basedir}
 export PICO_SDK_PATH=${PICO_BASE}/pico_sdk
 export PATH=$PATH:${PICO_BASE}/pico_tools/bin
-export PICOTOOL_FETCH_FROM_GIT_PATH=${PICO_BASE}/pico_tools/bin
+export PICOTOOL_FETCH_FROM_GIT_PATH=${PICO_BASE}/builds/picotool_build
 EOF
 
 # Install cross-compiler 
@@ -77,6 +84,5 @@ echo "   binutils-arm-none-eabi gcc-arm-none-eabi newlib-arm-none-eabi gdb-multi
 echo ""
 
 echo "** UDEV Rules **"
-echo " install 99-picotool.rules /etc/udev/rules.d then udevadm control --reload-rules "
+echo " install 99-picotool.rules /etc/udev/rules.d then reboot"
 echo "!! Script Complete.. !!"
-
